0da6b04eec27fc392c92a7cc9cde2ccbe7047121
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 14:56:09 -0700

    Update ex_4_3.py
    Done

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -1,5 +1,7 @@
 """ ex_4_3.py """
+
 import os
+from datetime import timedelta
 
 try:
     from src.ex_4_0 import get_shutdown_events
@@ -14,33 +16,28 @@ except ImportError:
 FILENAME = get_data_file_path("messages.log")
 # >>>> DO NOT MODIFY CODE ABOVE <<<<
 
-
 def time_between_shutdowns(logfile):
     """
-    Calculate the time duration between the first and last shutdowns in the log file.
+    Calculate the time between the first and last shutdown events in the log file.
 
-    Args:
-        logfile (str): The path to the log file.
-
-    Returns:
-        datetime.timedelta: The time duration between the first and last shutdowns.
+    :param logfile: The filename of the log file.
+    :return: A timedelta representing the time between the first and last shutdown events.
     """
     shutdown_events = get_shutdown_events(logfile)
-    
-    if len(shutdown_events) < 2:
-        # If there are less than 2 shutdown events, return a zero timedelta.
-        return timedelta(seconds=0)
-    
-    # Convert the date fields of the first and last shutdown events to datetime objects.
-    first_shutdown_time = logstamp_to_datetime(shutdown_events[0]['date'])
-    last_shutdown_time = logstamp_to_datetime(shutdown_events[-1]['date'])
-    
-    # Calculate the time duration between the first and last shutdowns.
-    time_duration = last_shutdown_time - first_shutdown_time
-    
-    return time_duration
+    if not shutdown_events:
+        return timedelta()  # No shutdown events found, return a zero timedelta.
 
-# The code below will call your function and print the results
+    # Convert the date fields to datetime objects
+    shutdown_events = [logstamp_to_datetime(entry['date']) for entry in shutdown_events]
+
+    # Sort the events by timestamp
+    shutdown_events.sort()
+
+    # Calculate the time difference between the first and last shutdown events
+    time_difference = shutdown_events[-1] - shutdown_events[0]
+
+    return time_difference
+
+# >>>> The code below will call your function and print the results
 if __name__ == "__main__":
-    from datetime import timedelta
     print(f'{time_between_shutdowns(FILENAME)=}')
