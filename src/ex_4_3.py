62b94f1722c17e53b214a05e8c1572251471e38f
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 14:50:53 -0700

    Update ex_4_3.py
    Done

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -17,11 +17,30 @@ FILENAME = get_data_file_path("messages.log")
 
 def time_between_shutdowns(logfile):
     """
-    Your docstring here.  Replace the pass keyword below with your implementation.
+    Calculate the time duration between the first and last shutdowns in the log file.
+
+    Args:
+        logfile (str): The path to the log file.
+
+    Returns:
+        datetime.timedelta: The time duration between the first and last shutdowns.
     """
-    pass
+    shutdown_events = get_shutdown_events(logfile)
+    
+    if len(shutdown_events) < 2:
+        # If there are less than 2 shutdown events, return a zero timedelta.
+        return timedelta(seconds=0)
+    
+    # Convert the date fields of the first and last shutdown events to datetime objects.
+    first_shutdown_time = logstamp_to_datetime(shutdown_events[0]['date'])
+    last_shutdown_time = logstamp_to_datetime(shutdown_events[-1]['date'])
+    
+    # Calculate the time duration between the first and last shutdowns.
+    time_duration = last_shutdown_time - first_shutdown_time
+    
+    return time_duration
 
-
-# >>>> The code below will call your function and print the results
+# The code below will call your function and print the results
 if __name__ == "__main__":
+    from datetime import timedelta
     print(f'{time_between_shutdowns(FILENAME)=}')
