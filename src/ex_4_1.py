bea27130a20aa03feda8f2b2cfd49734a80dc5a0
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 21:54:27 -0700

    Update ex_4_1.py
    Complete

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -8,22 +8,13 @@ except ImportError:
     from ex_4_0 import get_shutdown_events
     from util import get_data_file_path
 
-# Use this FILENAME variable to test your function.
+
 FILENAME = get_data_file_path('messages.log')
 
+
 def num_shutdowns(logfile):
-    """
-    Counts the number of shutdown events in the given log file.
-
-    Args:
-        logfile (str): The path to the log file.
-
-    Returns:
-        int: The number of shutdown events in the log file.
-    """
     shutdown_events = get_shutdown_events(logfile)
     return len(shutdown_events)
 
-# The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{num_shutdowns(FILENAME)=}')
