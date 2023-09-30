a95c7498b156af4186228d163753d4255950026b
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 21:54:34 -0700

    Update ex_4_2.py
    Complete

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -1,19 +1,16 @@
-# ex_4_2.py
+""" ex_4_2.py """
 from datetime import datetime
 
+
 def logstamp_to_datetime(datestr):
-    """
-    Converts a date string in the format 'YYYY-MM-DDTHH:MM:SS' to a datetime object.
+    try:
+      
+        return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+       
+        return datetime.strptime(datestr + '.000000', '%Y-%m-%dT%H:%M:%S.%f')
 
-    Args:
-        datestr (str): The date string to convert.
 
-    Returns:
-        datetime.datetime: A datetime object representing the input date and time.
-    """
-    return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
-
-# The code below will call your function and print the results
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
     print(f'{logstamp_to_datetime(test_date)=}')
