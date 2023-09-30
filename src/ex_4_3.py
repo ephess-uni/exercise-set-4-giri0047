cacceefbcf386a5751cbf484da2db2d243511a37
giri0047 <143857761+giri0047@users.noreply.github.com>
2023-09-29 21:54:41 -0700

    Update ex_4_3.py
    Complete

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -10,35 +10,20 @@ except ImportError:
     from ex_4_2 import logstamp_to_datetime
     from util import get_data_file_path
 
-# Use this FILENAME variable to test your function.
+
 FILENAME = get_data_file_path("messages.log")
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
+
 
 
 def time_between_shutdowns(logfile):
-    """
-    Calculate the amount of time between the first and last shutdowns in a log file.
-
-    Args:
-    logfile (str): The filename of the log file.
-
-    Returns:
-    timedelta: The time duration between the first and last shutdowns.
-    """
-    shutdown_events = get_shutdown_events(logfile)
     
-    if not shutdown_events:
-        return timedelta()  # Return a zero timedelta if there are no shutdown events.
+    shutdown_events = get_shutdown_events(logfile)
 
-    # Convert the date fields to datetime objects for the first and last shutdown events.
-    first_shutdown_time = logstamp_to_datetime(shutdown_events[0]['date'])
-    last_shutdown_time = logstamp_to_datetime(shutdown_events[-1]['date'])
+    first_shutdown = logstamp_to_datetime(shutdown_events[0].split()[1])
+    last_shutdown = logstamp_to_datetime(shutdown_events[-1].split()[1])
 
-    # Compute the time difference and ensure it's a positive value.
-    time_difference = abs(last_shutdown_time - first_shutdown_time)
+   
+    return abs(last_shutdown - first_shutdown)
 
-    return time_difference
-
-# >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
